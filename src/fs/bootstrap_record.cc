2a17c1e452c3bb0b1573507ca4858b7ff59ec2f9
Michał Niciejewski <quport@gmail.com>
2019-12-11 01:13:15 +0100

    Modified bootstrap_record so it has fixed size

--- src/fs/bootstrap_record.cc
+++ src/fs/bootstrap_record.cc
@@ -29,8 +29,26 @@ namespace seastar::fs {
 namespace {
 
 constexpr size_t alignment = 4 * KB;
-constexpr uint32_t boot_cluster_size = 1 * MB;
-constexpr uint64_t boot_cluster_offset = 0;
+constexpr uint64_t boot_record_offset = 0;
+constexpr uint32_t shards_nb = 4;
+
+uint32_t crc32(const char* buff, size_t len) {
+    boost::crc_32_type result;
+    result.process_bytes(buff, len);
+    return result.checksum();
+}
+
+struct bootstrap_record_disk {
+    uint64_t _magic;
+    uint64_t _version;
+    uint32_t _sector_size;
+    uint32_t _cluster_size;
+    uint32_t _root_id;
+    uint32_t _shards_nb;
+    uint64_t _metadata_ptr[shards_nb];
+    uint32_t _crc;
+    char _padding[4]; // TODO(fs): try remove that padding, maybe pack?
+};
 
 }
 
@@ -42,70 +60,39 @@ future<bootstrap_record> bootstrap_record::create_from_memory() {
     throw std::bad_function_call();
 }
 
-namespace {
-
-class record_writer {
-    char* _buff;
-    uint32_t _offset = 0;
-    uint32_t _max_size;
-
-public:
-    record_writer(char* buff, uint32_t max_size)
-        : _buff(buff), _max_size(max_size) {}
-
-    uint32_t get_len() const {
-        return _offset;
-    }
-
-    const char* get_buff() const {
-        return _buff;
-    }
-
-    void add_elem(uint32_t elem) {
-        // TODO(fs): add max_size check
-        elem = htobe32(elem);
-        std::memcpy(_buff + _offset, &elem, sizeof(elem));
-        _offset += sizeof(elem);
-    }
-
-    void add_elem(uint64_t elem) {
-        // TODO(fs): add max_size check
-        elem = htobe64(elem);
-        std::memcpy(_buff + _offset, &elem, sizeof(elem));
-        _offset += sizeof(elem);
-    }
-
-    void add_elem(const std::vector<uint64_t>& elem) {
-        // TODO(fs): add max_size check
-        for (auto&& i : elem)
-            add_elem(i);
-    }
-};
-
-uint32_t crc32(const char* buff, size_t len) {
-    boost::crc_32_type result;
-    result.process_bytes(buff, len);
-    return result.checksum();
-}
-
-}
-
 future<> bootstrap_record::write_to_disk(block_device& device) {
-    auto boot_cluster = temporary_buffer<char>::aligned(alignment, boot_cluster_size);
-    record_writer writer(boot_cluster.get_write(), boot_cluster_size);
+    bootstrap_record_disk boot_record;
 
-    // TODO(fs): add error handling
-    writer.add_elem(_magic);
-    writer.add_elem(_version);
-    writer.add_elem(_sector_size);
-    writer.add_elem(_cluster_size);
-    writer.add_elem(_root_id);
-    writer.add_elem(_shards_nb);
-    writer.add_elem(_metadata_ptr);
-    writer.add_elem(crc32(writer.get_buff(), writer.get_len()));
+    size_t boot_record_size = sizeof(boot_record);
+    size_t aligned_boot_record_size = boot_record_size % alignment
+            ? boot_record_size + alignment - boot_record_size % alignment
+            : boot_record_size;
+    auto boot_record_buff = temporary_buffer<char>::aligned(alignment, aligned_boot_record_size);
+    std::memset(boot_record_buff.get_write(), 0, aligned_boot_record_size);
 
-    return device.write(boot_cluster_offset, boot_cluster.get(), boot_cluster_size)
-            .then([boot_cluster = std::move(boot_cluster)] (size_t ret) {
+    // prepare bootstrap_record_disk records
+    boot_record._magic = htobe64(_magic);
+    boot_record._version = htobe64(_version);
+    boot_record._sector_size = htobe32(_sector_size);
+    boot_record._cluster_size = htobe32(_cluster_size);
+    boot_record._root_id = htobe32(_root_id);
+    boot_record._shards_nb = htobe32(_shards_nb);
+    for (size_t i = 0; i < _metadata_ptr.size(); i++)
+        boot_record._metadata_ptr[i] = htobe64(_metadata_ptr[i]);
+    for (size_t i = _metadata_ptr.size(); i < shards_nb; i++)
+        boot_record._metadata_ptr[i] = 0;
+    boot_record._crc = 0;
+
+    std::memcpy(boot_record_buff.get_write(), &boot_record, sizeof(boot_record));
+
+    // calculate crc
+    size_t crc_offset = offsetof(bootstrap_record_disk, _crc);
+    uint32_t crc = crc32(boot_record_buff.get(), crc_offset);
+    boot_record._crc = htobe32(crc);
+    std::memcpy(boot_record_buff.get_write() + crc_offset, &boot_record._crc, sizeof(boot_record._crc));
+
+    return device.write(boot_record_offset, boot_record_buff.get(), aligned_boot_record_size)
+            .then([boot_record_buff = std::move(boot_record_buff)] (size_t ret) {
         // TODO(fs): handle case where ret != boot_cluster_size
         return make_ready_future<>();
     });
