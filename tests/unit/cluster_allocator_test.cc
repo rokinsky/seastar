0b47e08c1412d80b61524e7d7e8910dc1b8f3273
Krzysztof Małysa <varqox@gmail.com>
2020-01-04 18:47:08 +0100

    fs: Refactored cluster_allocator

--- tests/unit/cluster_allocator_test.cc
+++ tests/unit/cluster_allocator_test.cc
@@ -25,61 +25,69 @@
 
 #include <boost/test/included/unit_test.hpp>
 #include <deque>
-#include <queue>
 #include <seastar/core/units.hh>
 #include <unordered_set>
 
 using namespace seastar;
 
+BOOST_AUTO_TEST_CASE(cluster_allocator_cluster_0) {
+    fs::cluster_allocator ca({}, {0});
+    BOOST_REQUIRE_EQUAL(ca.alloc().value(), 0);
+    BOOST_REQUIRE(ca.alloc() == std::nullopt);
+    BOOST_REQUIRE(ca.alloc() == std::nullopt);
+    ca.free(0);
+    BOOST_REQUIRE_EQUAL(ca.alloc().value(), 0);
+    BOOST_REQUIRE(ca.alloc() == std::nullopt);
+    BOOST_REQUIRE(ca.alloc() == std::nullopt);
+}
 
 BOOST_AUTO_TEST_CASE(cluster_allocator) {
     constexpr fs::cluster_id_t clusters_per_shard = 1024;
     std::unordered_set<fs::cluster_id_t> empty_uset;
-    std::queue<fs::cluster_id_t> empty_queue;
-    fs::cluster_allocator empty_ca{empty_uset, empty_queue};
-    BOOST_REQUIRE_EQUAL(empty_ca.alloc(), 0);
+    fs::cluster_allocator empty_ca{empty_uset, {}};
+    BOOST_REQUIRE(empty_ca.alloc() == std::nullopt);
     std::deque<fs::cluster_id_t> deq{1, 5, 3, 4, 2};
-    fs::cluster_allocator small_ca(empty_uset, std::queue<fs::cluster_id_t>{deq});
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[0]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[1]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[2]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[3]);
+    fs::cluster_allocator small_ca(empty_uset, deq);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[0]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[1]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[2]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[3]);
     small_ca.free(deq[2]);
     small_ca.free(deq[1]);
     small_ca.free(deq[3]);
     small_ca.free(deq[0]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[4]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[2]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[1]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[3]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[0]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), 0);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[4]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[2]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[1]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[3]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[0]);
+    BOOST_REQUIRE(small_ca.alloc() == std::nullopt);
     small_ca.free(deq[2]);
     small_ca.free(deq[4]);
     small_ca.free(deq[3]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[2]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[4]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[2]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[4]);
     small_ca.free(deq[2]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[3]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[3]);
     small_ca.free(deq[4]);
-    BOOST_REQUIRE_EQUAL(small_ca.alloc(), deq[2]);
+    BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[2]);
 
-    std::queue<fs::cluster_id_t> q;
+    std::deque<fs::cluster_id_t> dq;
     for (fs::cluster_id_t i = 0; i < clusters_per_shard; i++) {
-        q.push(i);
+        dq.emplace_back(i);
     }
-    fs::cluster_allocator ordinary_ca(empty_uset, q);
+    fs::cluster_allocator ordinary_ca(empty_uset, dq);
     for (fs::cluster_id_t i = 0; i < clusters_per_shard; i++) {
-        BOOST_REQUIRE_EQUAL(ordinary_ca.alloc(), i);
+        BOOST_REQUIRE_EQUAL(ordinary_ca.alloc().value(), i);
     }
     for (fs::cluster_id_t i = 0; i < clusters_per_shard; i++) {
         ordinary_ca.free(i);
     }
     std::unordered_set<fs::cluster_id_t> uset;
-    std::queue<fs::cluster_id_t>().swap(q);
+    dq.clear();
     fs::cluster_id_t elem = 215;
     while (elem != 806) {
-        q.push(elem);
+        dq.emplace_back(elem);
         elem = (elem * 215) % 1021;
     }
     elem = 1;
@@ -87,10 +95,10 @@ BOOST_AUTO_TEST_CASE(cluster_allocator) {
         uset.insert(elem);
         elem = (elem * 19) % 1021;
     }
-    fs::cluster_allocator random_ca(uset, q);
+    fs::cluster_allocator random_ca(uset, dq);
     elem = 215;
     while (elem != 1) {
-        BOOST_REQUIRE_EQUAL(random_ca.alloc(), elem);
+        BOOST_REQUIRE_EQUAL(random_ca.alloc().value(), elem);
         random_ca.free(1021-elem);
         elem = (elem * 215) % 1021;
     }
