25e70061829ef26ab9cf852edcd966fdae075588
Wojciech Mitros <44506288+wmitros@users.noreply.github.com>
2020-01-08 00:23:31 +0100

    fs: tests: fixed formatting and naming in cluster allocator tests
    Co-Authored-By: Krzysztof Małysa <varqox@gmail.com>

--- tests/unit/fs_cluster_allocator_test.cc
+++ tests/unit/fs_cluster_allocator_test.cc
@@ -30,7 +30,7 @@
 
 using namespace seastar;
 
-BOOST_AUTO_TEST_CASE(cluster_allocator_cluster_0) {
+BOOST_AUTO_TEST_CASE(test_cluster_0) {
     fs::cluster_allocator ca({}, {0});
     BOOST_REQUIRE_EQUAL(ca.alloc().value(), 0);
     BOOST_REQUIRE(ca.alloc() == std::nullopt);
@@ -41,20 +41,19 @@ BOOST_AUTO_TEST_CASE(cluster_allocator_cluster_0) {
     BOOST_REQUIRE(ca.alloc() == std::nullopt);
 }
 
-BOOST_AUTO_TEST_CASE(cluster_allocator_empty) {
-    std::unordered_set<fs::cluster_id_t> empty_uset;
-    fs::cluster_allocator empty_ca{empty_uset, {}};
+BOOST_AUTO_TEST_CASE(test_empty) {
+    fs::cluster_allocator empty_ca{{}, {}};
     BOOST_REQUIRE(empty_ca.alloc() == std::nullopt);
 }
 
-BOOST_AUTO_TEST_CASE(cluster_allocator_small) {
-    std::unordered_set<fs::cluster_id_t> empty_uset;
+BOOST_AUTO_TEST_CASE(test_small) {
     std::deque<fs::cluster_id_t> deq{1, 5, 3, 4, 2};
-    fs::cluster_allocator small_ca(empty_uset, deq);
+    fs::cluster_allocator small_ca({}, deq);
     BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[0]);
     BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[1]);
     BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[2]);
     BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[3]);
+
     small_ca.free(deq[2]);
     small_ca.free(deq[1]);
     small_ca.free(deq[3]);
@@ -65,6 +64,7 @@ BOOST_AUTO_TEST_CASE(cluster_allocator_small) {
     BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[3]);
     BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[0]);
     BOOST_REQUIRE(small_ca.alloc() == std::nullopt);
+
     small_ca.free(deq[2]);
     small_ca.free(deq[4]);
     small_ca.free(deq[3]);
@@ -76,27 +76,28 @@ BOOST_AUTO_TEST_CASE(cluster_allocator_small) {
     BOOST_REQUIRE_EQUAL(small_ca.alloc().value(), deq[2]);
 }
 
-BOOST_AUTO_TEST_CASE(cluster_allocator_max) {
+BOOST_AUTO_TEST_CASE(test_max) {
     constexpr fs::cluster_id_t clusters_per_shard = 1024;
-    std::deque<fs::cluster_id_t> dq;
-    std::unordered_set<fs::cluster_id_t> empty_uset;
+    std::deque<fs::cluster_id_t> deq;
     for (fs::cluster_id_t i = 0; i < clusters_per_shard; i++) {
-        dq.emplace_back(i);
+        deq.emplace_back(i);
     }
-    fs::cluster_allocator ordinary_ca(empty_uset, dq);
+    fs::cluster_allocator ordinary_ca({}, deq);
     for (fs::cluster_id_t i = 0; i < clusters_per_shard; i++) {
         BOOST_REQUIRE_EQUAL(ordinary_ca.alloc().value(), i);
     }
+    BOOST_REQUIRE(ordinary_ca.alloc() == std::nullopt);
     for (fs::cluster_id_t i = 0; i < clusters_per_shard; i++) {
         ordinary_ca.free(i);
     }
 }
-BOOST_AUTO_TEST_CASE(cluster_allocator_pseudo_rand) {
+
+BOOST_AUTO_TEST_CASE(test_pseudo_rand) {
     std::unordered_set<fs::cluster_id_t> uset;
-    std::deque<fs::cluster_id_t> dq;
+    std::deque<fs::cluster_id_t> deq;
     fs::cluster_id_t elem = 215;
     while (elem != 806) {
-        dq.emplace_back(elem);
+        deq.emplace_back(elem);
         elem = (elem * 215) % 1021;
     }
     elem = 1;
@@ -104,7 +105,7 @@ BOOST_AUTO_TEST_CASE(cluster_allocator_pseudo_rand) {
         uset.insert(elem);
         elem = (elem * 19) % 1021;
     }
-    fs::cluster_allocator random_ca(uset, dq);
+    fs::cluster_allocator random_ca(uset, deq);
     elem = 215;
     while (elem != 1) {
         BOOST_REQUIRE_EQUAL(random_ca.alloc().value(), elem);
