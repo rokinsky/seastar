59540ed2a7397732fef4f3fc541b9f59476a9d7c
Krzysztof Małysa <varqox@gmail.com>
2020-01-08 01:10:12 +0100

    fs: moved metadata_disk_* to a separate header file and renamed to_disk_buffer to metadata_to_disk_buffer with moving it to a separate header file

---
+++ src/fs/metadata_to_disk_buffer.hh
@@ -0,0 +1,266 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Copyright (C) 2019 ScyllaDB
+ */
+
+#pragma once
+
+#include "metadata_disk_entries.hh"
+#include "seastar/core/future.hh"
+#include "seastar/core/temporary_buffer.hh"
+#include "seastar/fs/block_device.hh"
+
+#include <boost/crc.hpp>
+#include <cstring>
+
+namespace seastar::fs {
+
+class metadata_to_disk_buffer {
+    temporary_buffer<uint8_t> _buff;
+    const unit_size_t _alignment;
+    disk_offset_t _disk_write_offset; // disk offset that corresponds to _buff.begin()
+    range<size_t> _next_write; // range of unflushed bytes in _buff, beg is aligned to _alignment
+    size_t _zero_padded_end; // Optimization to skip padding before write if it is already done
+    boost::crc_32_type _crc;
+
+public:
+    // Represents buffer that will be written to a block_device at offset @p disk_alligned_write_offset. Total number of bytes appended cannot exceed @p aligned_max_size.
+    metadata_to_disk_buffer(size_t aligned_max_size, unit_size_t alignment, disk_offset_t disk_aligned_write_offset)
+    : _buff(decltype(_buff)::aligned(alignment, aligned_max_size))
+    , _alignment(alignment)
+    , _disk_write_offset(disk_aligned_write_offset)
+    , _next_write {0, 0}
+    , _zero_padded_end(0) {
+        assert(is_power_of_2(alignment));
+        assert(mod_by_power_of_2(disk_aligned_write_offset, alignment) == 0);
+        assert(aligned_max_size % alignment == 0);
+        assert(aligned_max_size >= sizeof(ondisk_type) + sizeof(ondisk_checkpoint));
+        start_new_write();
+    }
+
+    // Clears buffer, leaving it in state as if it was just constructed
+    void reset(disk_offset_t new_disk_aligned_write_offset) noexcept {
+        assert(mod_by_power_of_2(new_disk_aligned_write_offset, _alignment) == 0);
+        _disk_write_offset = new_disk_aligned_write_offset;
+        _next_write = {0, 0};
+        _zero_padded_end = 0;
+        start_new_write();
+    }
+
+    // Clears buffer, leaving it in state as if it was just constructed
+    void reset_from_bootstraped_cluster(disk_offset_t cluster_offset, const uint8_t* cluster_contents, size_t metadata_end_pos) noexcept {
+        assert(mod_by_power_of_2(cluster_offset, _alignment) == 0);
+        _disk_write_offset = cluster_offset;
+        _next_write = {
+            metadata_end_pos - mod_by_power_of_2(metadata_end_pos, _alignment),
+            metadata_end_pos
+        };
+        memcpy(_buff.get_write() + _next_write.beg, cluster_contents + _next_write.beg, _next_write.size());
+        _zero_padded_end = 0;
+        start_new_write();
+    }
+
+    // Writes buffered data to disk
+    // IMPORTANT: using this buffer before call completes is UB
+    future<> flush_to_disk(block_device device) {
+        make_checkpoint_vaild();
+        // Pad buffer with zeros till alignment. Layout overview:
+        // | .....................|....................|00000000000000000000|
+        // ^ _next_write.beg      ^ new_next_write_beg ^ _next_write.end    ^ aligned_end
+        //      (aligned)               (aligned)       (maybe unaligned)      (aligned)
+        //                        |<-------------- _alignment ------------->|
+        size_t new_next_write_beg = _next_write.end - mod_by_power_of_2(_next_write.end, _alignment);
+        size_t aligned_end = new_next_write_beg + _alignment;
+        if (_zero_padded_end != aligned_end) {
+            range padding = {_next_write.end, aligned_end};
+            memset(_buff.get_write() + padding.beg, 0, padding.size());
+            _zero_padded_end = padding.end;
+        }
+        range true_write = {_next_write.beg, aligned_end};
+
+        return device.write(_disk_write_offset + true_write.beg, _buff.get_write() + true_write.beg, true_write.size()).then([this, true_write, new_next_write_beg](size_t written_bytes) {
+            if (written_bytes != true_write.size()) {
+                return make_exception_future<>(std::runtime_error("Partial write"));
+            }
+
+            _next_write = {new_next_write_beg, new_next_write_beg};
+            start_new_write();
+            return now();
+        });
+    }
+
+private:
+    void start_new_write() noexcept {
+        ondisk_type type = INVALID;
+        ondisk_checkpoint checkpoint;
+        memset(&checkpoint, 0, sizeof(checkpoint));
+
+        assert(bytes_left() >= sizeof(type) + sizeof(checkpoint));
+        append_bytes(&type, sizeof(type));
+        append_bytes(&checkpoint, sizeof(checkpoint));
+
+        _crc.reset();
+    }
+
+    void make_checkpoint_vaild() noexcept {
+        // Make checkpoint valid
+        ondisk_type checkpoint_type = CHECKPOINT;
+        size_t checkpoint_pos = _next_write.beg + sizeof(checkpoint_type);
+        ondisk_checkpoint checkpoint;
+        checkpoint.checkpointed_data_length = _next_write.end - checkpoint_pos - sizeof(checkpoint);
+        _crc.process_bytes(&checkpoint.checkpointed_data_length, sizeof(checkpoint.checkpointed_data_length));
+        checkpoint.crc32_code = _crc.checksum();
+
+        memcpy(_buff.get_write() + _next_write.beg, &checkpoint_type, sizeof(checkpoint_type));
+        memcpy(_buff.get_write() + checkpoint_pos, &checkpoint, sizeof(checkpoint));
+    }
+
+    void append_bytes(const void* data, size_t len) noexcept {
+        assert(len <= bytes_left());
+        _crc.process_bytes(data, len);
+        memcpy(_buff.get_write() + _next_write.end, data, len);
+        _next_write.end += len;
+    }
+
+public:
+    // Returns maximum number of bytes that may be written to buffer without calling reset()
+    size_t bytes_left() const noexcept { return _buff.size() - _next_write.end; }
+
+    enum append_result {
+        APPENDED,
+        TOO_BIG,
+    };
+
+    append_result append(const ondisk_next_metadata_cluster& next_metadata_cluster) noexcept {
+        ondisk_type type = NEXT_METADATA_CLUSTER;
+        if (bytes_left() < sizeof(type) + sizeof(next_metadata_cluster)) {
+            return TOO_BIG;
+        }
+
+        append_bytes(&type, sizeof(type));
+        append_bytes(&next_metadata_cluster, sizeof(next_metadata_cluster));
+        return APPENDED;
+    }
+
+private:
+    bool fits_for_append(size_t bytes_no) const noexcept {
+        return (bytes_left() >= bytes_no + sizeof(ondisk_next_metadata_cluster)); // We need to reserve space for the next metadata cluster entry
+    }
+
+    template<class T>
+    append_result append_simple(ondisk_type type, const T& entry) noexcept {
+        size_t total_size = sizeof(type) + sizeof(entry);
+        if (not fits_for_append(total_size)) {
+            return TOO_BIG;
+        }
+
+        append_bytes(&type, sizeof(type));
+        append_bytes(&entry, sizeof(entry));
+        return APPENDED;
+    }
+
+public:
+    append_result append(const ondisk_create_inode& create_inode) noexcept {
+        // TODO: maybe add a constexpr static field to each ondisk_* entry specifying what type it is?
+        return append_simple(CREATE_INODE, create_inode);
+    }
+
+    append_result append(const ondisk_update_metadata& update_metadata) noexcept {
+        return append_simple(UPDATE_METADATA, update_metadata);
+    }
+
+    append_result append(const ondisk_delete_inode& delete_inode) noexcept {
+        return append_simple(DELETE_INODE, delete_inode);
+    }
+
+    append_result append(const ondisk_medium_write& medium_write) noexcept {
+        return append_simple(MEDIUM_WRITE, medium_write);
+    }
+
+    append_result append(const ondisk_large_write& large_write) noexcept {
+        return append_simple(LARGE_WRITE, large_write);
+    }
+
+    append_result append(const ondisk_large_write_without_mtime& large_write_without_mtime) noexcept {
+        return append_simple(LARGE_WRITE_WITHOUT_MTIME, large_write_without_mtime);
+    }
+
+    append_result append(const ondisk_truncate& truncate) noexcept {
+        return append_simple(TRUNCATE, truncate);
+    }
+
+    append_result append(const ondisk_mtime_update& mtime_update) noexcept {
+        return append_simple(MTIME_UPDATE, mtime_update);
+    }
+
+    append_result append(const ondisk_small_write_header& small_write, const void* data) noexcept {
+        ondisk_type type = SMALL_WRITE;
+        size_t total_size = sizeof(type) + sizeof(small_write) + small_write.length;
+        if (not fits_for_append(total_size)) {
+            return TOO_BIG;
+        }
+
+        append_bytes(&type, sizeof(type));
+        append_bytes(&small_write, sizeof(small_write));
+        append_bytes(data, small_write.length);
+        return APPENDED;
+    }
+
+    append_result append(const ondisk_add_dir_entry_header& add_dir_entry, const void* entry_name) noexcept {
+        ondisk_type type = ADD_DIR_ENTRY;
+        size_t total_size = sizeof(type) + sizeof(add_dir_entry) + add_dir_entry.entry_name_length;
+        if (not fits_for_append(total_size)) {
+            return TOO_BIG;
+        }
+
+        append_bytes(&type, sizeof(type));
+        append_bytes(&add_dir_entry, sizeof(add_dir_entry));
+        append_bytes(entry_name, add_dir_entry.entry_name_length);
+        return APPENDED;
+    }
+
+    append_result append(const ondisk_rename_dir_entry_header& rename_dir_entry, const void* old_name, const void* new_name) noexcept {
+        ondisk_type type = RENAME_DIR_ENTRY;
+        size_t total_size = sizeof(type) + sizeof(rename_dir_entry) + rename_dir_entry.entry_old_name_length + rename_dir_entry.entry_new_name_length;
+        if (not fits_for_append(total_size)) {
+            return TOO_BIG;
+        }
+
+        append_bytes(&type, sizeof(type));
+        append_bytes(&rename_dir_entry, sizeof(rename_dir_entry));
+        append_bytes(old_name, rename_dir_entry.entry_old_name_length);
+        append_bytes(new_name, rename_dir_entry.entry_new_name_length);
+        return APPENDED;
+    }
+
+    append_result append(const ondisk_delete_dir_entry_header& delete_dir_entry, const void* entry_name) noexcept {
+        ondisk_type type = DELETE_DIR_ENTRY;
+        size_t total_size = sizeof(type) + sizeof(delete_dir_entry) + delete_dir_entry.entry_name_length;
+        if (not fits_for_append(total_size)) {
+            return TOO_BIG;
+        }
+
+        append_bytes(&type, sizeof(type));
+        append_bytes(&delete_dir_entry, sizeof(delete_dir_entry));
+        append_bytes(entry_name, delete_dir_entry.entry_name_length);
+        return APPENDED;
+    }
+};
+
+} // namespace seastar::fs
