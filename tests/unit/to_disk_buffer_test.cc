36a99743fb0541b4d9ab917cb433f6d782ace05b
wmitros <han200@wp.pl>
2020-02-18 00:46:36 +0100

    tests: fixed to_disk_buffer unit tests

--- tests/unit/to_disk_buffer_test.cc
+++ tests/unit/to_disk_buffer_test.cc
@@ -50,7 +50,7 @@ SEASTAR_THREAD_TEST_CASE(simple_unaligned_write_test) {
     auto dev_impl = make_shared<mock_block_device_impl>();
     block_device dev(dev_impl);
     auto disk_buf = to_disk_buffer(max_siz, alignment, 0);
-    char buf[max_siz];
+    auto buf = temporary_buffer<char>::aligned(alignment, max_siz);
     disk_offset_t len_aligned;
     disk_buf.append_bytes("asdfasdf", 6);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-6);
@@ -59,38 +59,40 @@ SEASTAR_THREAD_TEST_CASE(simple_unaligned_write_test) {
     disk_buf.flush_to_disk(dev, false).get();
     len_aligned = round_up_to_multiple_of_power_of_2((disk_offset_t)15, alignment);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-15);
-    dev.read<char>(0, buf, len_aligned).get();
-    BOOST_REQUIRE_EQUAL(strncmp("asdfasxyzxyzxyz", buf, 15), 0);
+    dev.read<char>(0, buf.get_write(), len_aligned).get();
+    BOOST_REQUIRE_EQUAL(strncmp("asdfasxyzxyzxyz", buf.get(), 15), 0);
 }
 
 SEASTAR_THREAD_TEST_CASE(simple_aligned_write_test) {
     auto dev_impl = make_shared<mock_block_device_impl>();
     block_device dev(dev_impl);
     auto disk_buf = to_disk_buffer(max_siz, alignment, 0);
-    char buf[max_siz], expected_buf[max_siz];
+    auto buf = temporary_buffer<char>::aligned(alignment, max_siz);
+    auto expected_buf = temporary_buffer<char>::aligned(alignment, max_siz);
     disk_offset_t len_aligned;
     disk_buf.append_bytes("asdfasdf", 6);
     disk_buf.append_bytes("xyzxyzxyz", 9);
     disk_buf.flush_to_disk(dev, true).get();
     len_aligned = round_up_to_multiple_of_power_of_2((disk_offset_t)15, alignment);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-len_aligned);
-    dev.read<char>(0, buf, len_aligned).get();
-    strncpy(expected_buf, "asdfasxyzxyzxyz", max_siz);
-    BOOST_REQUIRE_EQUAL(strncmp(expected_buf, buf, len_aligned), 0);
+    dev.read<char>(0, buf.get_write(), len_aligned).get();
+    strncpy(expected_buf.get_write(), "asdfasxyzxyzxyz", max_siz);
+    BOOST_REQUIRE_EQUAL(strncmp(expected_buf.get(), buf.get(), len_aligned), 0);
 }
 
 SEASTAR_THREAD_TEST_CASE(unaligned_aligned_write_test) {
     auto dev_impl = make_shared<mock_block_device_impl>();
     block_device dev(dev_impl);
     auto disk_buf = to_disk_buffer(max_siz, alignment, 0);
-    char buf[max_siz], expected_buf[max_siz];
+    auto buf = temporary_buffer<char>::aligned(alignment, max_siz);
+    auto expected_buf = temporary_buffer<char>::aligned(alignment, max_siz);
     disk_offset_t len_aligned;
     disk_buf.append_bytes("asdfasdf", 6);
     disk_buf.append_bytes("xyzxyzxyz", 9);
     disk_buf.flush_to_disk(dev, false).get();
     len_aligned = round_up_to_multiple_of_power_of_2((disk_offset_t)15, alignment);
-    dev.read<char>(0, buf, len_aligned).get();
-    BOOST_REQUIRE_EQUAL(strncmp("asdfasxyzxyzxyz", buf, 15), 0);
+    dev.read<char>(0, buf.get_write(), len_aligned).get();
+    BOOST_REQUIRE_EQUAL(strncmp("asdfasxyzxyzxyz", buf.get(), 15), 0);
 
     disk_buf.append_bytes("1234", 4);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-19);
@@ -99,35 +101,36 @@ SEASTAR_THREAD_TEST_CASE(unaligned_aligned_write_test) {
     disk_buf.flush_to_disk(dev, true).get();
     len_aligned = round_up_to_multiple_of_power_of_2((disk_offset_t)28, alignment);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-len_aligned);
-    dev.read<char>(0, buf, len_aligned).get();
-    strncpy(expected_buf, "asdfasxyzxyzxyz1234aaaaaaaaa", max_siz);
-    BOOST_REQUIRE_EQUAL(strncmp(expected_buf, buf, len_aligned), 0);
+    dev.read<char>(0, buf.get_write(), len_aligned).get();
+    strncpy(expected_buf.get_write(), "asdfasxyzxyzxyz1234aaaaaaaaa", max_siz);
+    BOOST_REQUIRE_EQUAL(strncmp(expected_buf.get(), buf.get(), len_aligned), 0);
 }
 
 SEASTAR_THREAD_TEST_CASE(aligned_unaligned_write_test) {
     auto dev_impl = make_shared<mock_block_device_impl>();
     block_device dev(dev_impl);
     auto disk_buf = to_disk_buffer(max_siz, alignment, 0);
-    char buf[max_siz], expected_buf[max_siz];
+    auto buf = temporary_buffer<char>::aligned(alignment, max_siz);
+    auto expected_buf = temporary_buffer<char>::aligned(alignment, max_siz);
     disk_offset_t len_aligned, len_aligned2;
     disk_buf.append_bytes("1234", 4);
     disk_buf.append_bytes("aaaaaaaaa", 9);
     disk_buf.flush_to_disk(dev, true).get();
     len_aligned = round_up_to_multiple_of_power_of_2((disk_offset_t)13, alignment);
-    dev.read<char>(0, buf, len_aligned).get();
-    strncpy(expected_buf, "1234aaaaaaaaa", max_siz);
-    BOOST_REQUIRE_EQUAL(strncmp(expected_buf, buf, len_aligned), 0);
+    dev.read<char>(0, buf.get_write(), len_aligned).get();
+    strncpy(expected_buf.get_write(), "1234aaaaaaaaa", max_siz);
+    BOOST_REQUIRE_EQUAL(strncmp(expected_buf.get(), buf.get(), len_aligned), 0);
     
     disk_buf.append_bytes("asdfasdf", 6);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-len_aligned-6);
     disk_buf.append_bytes("xyzxyzxyz", 9);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-len_aligned-15);
     disk_buf.flush_to_disk(dev, false).get();
-    memcpy(expected_buf+len_aligned, "asdfasxyzxyzxyz", 15);
+    memcpy(expected_buf.get_write()+len_aligned, "asdfasxyzxyzxyz", 15);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-len_aligned-15);
     len_aligned2 = round_up_to_multiple_of_power_of_2(len_aligned+15, alignment);
-    dev.read<char>(0, buf, len_aligned2).get();
-    BOOST_REQUIRE_EQUAL(strncmp(expected_buf, buf, len_aligned+15), 0);
+    dev.read<char>(0, buf.get_write(), len_aligned2).get();
+    BOOST_REQUIRE_EQUAL(strncmp(expected_buf.get(), buf.get(), len_aligned+15), 0);
 }
 
 SEASTAR_THREAD_TEST_CASE(empty_aligned_write_test) {
@@ -160,7 +163,7 @@ SEASTAR_THREAD_TEST_CASE(reset_same_offset_test) {
     auto dev_impl = make_shared<mock_block_device_impl>();
     block_device dev(dev_impl);
     auto disk_buf = to_disk_buffer(max_siz, alignment, 0);
-    char buf[max_siz];
+    auto buf = temporary_buffer<char>::aligned(alignment, max_siz);
     disk_offset_t len_aligned;
     disk_buf.append_bytes("123450", 6);
     disk_buf.flush_to_disk(dev,false).get();
@@ -171,8 +174,8 @@ SEASTAR_THREAD_TEST_CASE(reset_same_offset_test) {
     disk_buf.flush_to_disk(dev,false).get();
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-9);
     len_aligned = round_up_to_multiple_of_power_of_2((disk_offset_t)9, alignment);
-    dev.read<char>(0, buf, len_aligned).get();
-    BOOST_REQUIRE_EQUAL(strncmp("overwrite", buf, 9), 0);
+    dev.read<char>(0, buf.get_write(), len_aligned).get();
+    BOOST_REQUIRE_EQUAL(strncmp("overwrite", buf.get(), 9), 0);
 
 }
 
@@ -180,7 +183,7 @@ SEASTAR_THREAD_TEST_CASE(reset_new_offset_test) {
     auto dev_impl = make_shared<mock_block_device_impl>();
     block_device dev(dev_impl);
     auto disk_buf = to_disk_buffer(max_siz, alignment, 0);
-    char buf[max_siz];
+    auto buf = temporary_buffer<char>::aligned(alignment, max_siz);
     disk_offset_t len_aligned;
     disk_buf.append_bytes("123450", 6);
     disk_buf.flush_to_disk(dev,false).get();
@@ -191,23 +194,24 @@ SEASTAR_THREAD_TEST_CASE(reset_new_offset_test) {
     disk_buf.flush_to_disk(dev,false).get();
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz-9);
     len_aligned = round_up_to_multiple_of_power_of_2((disk_offset_t)6, alignment);
-    dev.read<char>(0, buf, len_aligned).get();
-    BOOST_REQUIRE_EQUAL(strncmp("123450", buf, 6), 0);
+    dev.read<char>(0, buf.get_write(), len_aligned).get();
+    BOOST_REQUIRE_EQUAL(strncmp("123450", buf.get(), 6), 0);
     len_aligned = round_up_to_multiple_of_power_of_2((disk_offset_t)9, alignment);
-    dev.read<char>(6*alignment, buf, len_aligned).get();
-    BOOST_REQUIRE_EQUAL(strncmp("overwrite", buf, 9), 0);
+    dev.read<char>(6*alignment, buf.get_write(), len_aligned).get();
+    BOOST_REQUIRE_EQUAL(strncmp("overwrite", buf.get(), 9), 0);
 }
 
 SEASTAR_THREAD_TEST_CASE(combined_test) {
     auto dev_impl = make_shared<mock_block_device_impl>();
     block_device dev(dev_impl);
     auto disk_buf = to_disk_buffer(max_siz, alignment, 0);
-    char buf[max_siz], inp[max_siz];
-    strncpy(inp, "abcdefghiju8y7t6r5XXq1q2q3YYYY", max_siz); // fills to max_siz with null characters
+    auto buf = temporary_buffer<char>::aligned(alignment, max_siz);
+    auto inp = temporary_buffer<char>::aligned(alignment, max_siz);
+    strncpy(inp.get_write(), "abcdefghiju8y7t6r5XXq1q2q3YYYY", max_siz); // fills to max_siz with null characters
     disk_offset_t beg = 0, beg_aligned = 0, end = 0, end_aligned;
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz - end);
 
-    disk_buf.append_bytes(inp, 10);
+    disk_buf.append_bytes(inp.get(), 10);
     end += 10;
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz - end);
 
@@ -215,16 +219,16 @@ SEASTAR_THREAD_TEST_CASE(combined_test) {
     end_aligned = round_up_to_multiple_of_power_of_2(end, alignment);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz - end);
 
-    dev.read<char>(beg_aligned, buf, end_aligned-beg_aligned).get();
-    BOOST_REQUIRE_EQUAL(strncmp(inp, buf+beg-beg_aligned, end-beg), 0);
+    dev.read<char>(beg_aligned, buf.get_write(), end_aligned-beg_aligned).get();
+    BOOST_REQUIRE_EQUAL(strncmp(inp.get(), buf.get()+beg-beg_aligned, end-beg), 0);
 
     beg = end;
     beg_aligned = round_down_to_multiple_of_power_of_2(beg, alignment);
-    disk_buf.append_bytes(inp+10, 8);
+    disk_buf.append_bytes(inp.get()+10, 8);
     end += 8;
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz - end);
 
-    disk_buf.append_bytes(inp+20, 6);
+    disk_buf.append_bytes(inp.get()+20, 6);
     end += 6;
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz - end);
 
@@ -232,13 +236,13 @@ SEASTAR_THREAD_TEST_CASE(combined_test) {
     end_aligned = round_up_to_multiple_of_power_of_2(end, alignment);
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz - end_aligned);
 
-    dev.read<char>(beg_aligned, buf, end_aligned-beg_aligned).get();
-    BOOST_REQUIRE_EQUAL(strncmp(inp+10, buf+beg-beg_aligned, 8), 0);
-    BOOST_REQUIRE_EQUAL(strncmp(inp+20, buf+beg-beg_aligned+8, 6), 0);
-    BOOST_REQUIRE_EQUAL(strncmp(inp+30, buf+beg-beg_aligned+14, end_aligned-end), 0);
+    dev.read<char>(beg_aligned, buf.get_write(), end_aligned-beg_aligned).get();
+    BOOST_REQUIRE_EQUAL(strncmp(inp.get_write()+10, buf.get()+beg-beg_aligned, 8), 0);
+    BOOST_REQUIRE_EQUAL(strncmp(inp.get_write()+20, buf.get()+beg-beg_aligned+8, 6), 0);
+    BOOST_REQUIRE_EQUAL(strncmp(inp.get_write()+30, buf.get()+beg-beg_aligned+14, end_aligned-end), 0);
     
     end = end_aligned;
-    disk_buf.append_bytes(inp+26, 4);
+    disk_buf.append_bytes(inp.get()+26, 4);
     end += 4;
     BOOST_REQUIRE_EQUAL(disk_buf.bytes_left(), max_siz - end);
 
