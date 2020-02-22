e9aa3f0396c1337552fa8c5a4543994b75354c49
Krzysztof Małysa <varqox@gmail.com>
2020-02-22 15:12:31 +0100

    fs: inode.hh: fixed invalid types and added shard_inode_allocator

--- src/fs/inode.hh
+++ src/fs/inode.hh
@@ -22,8 +22,10 @@
 #pragma once
 
 #include "bitwise.hh"
+#include "units.hh"
 
 #include <cstdint>
+#include <optional>
 
 namespace seastar::fs {
 
@@ -32,7 +34,7 @@ using inode_t = uint64_t;
 
 // Obtains shard id of the shard owning @p inode.
 //@p fs_shards_pool_size is the number of file system shards rounded up to a power of 2
-inline unsigned inode_to_shard_no(inode_t inode, unsigned fs_shards_pool_size) noexcept {
+inline fs_shard_id_t inode_to_shard_no(inode_t inode, fs_shard_id_t fs_shards_pool_size) noexcept {
     assert(is_power_of_2(fs_shards_pool_size));
     return mod_by_power_of_2(inode, fs_shards_pool_size);
 }
@@ -40,13 +42,39 @@ inline unsigned inode_to_shard_no(inode_t inode, unsigned fs_shards_pool_size) n
 // Returns inode belonging to the shard owning @p shard_previous_inode that is next after @p shard_previous_inode
 // (i.e. the lowest inode greater than @p shard_previous_inode belonging to the same shard)
 //@p fs_shards_pool_size is the number of file system shards rounded up to a power of 2
-inline inode_t shard_next_inode(inode_t shard_previous_inode, unsigned fs_shards_pool_size) noexcept {
+inline inode_t shard_next_inode(inode_t shard_previous_inode, fs_shard_id_t fs_shards_pool_size) noexcept {
     return shard_previous_inode + fs_shards_pool_size;
 }
 
-// Returns first inode (lowest by value) belonging to the shard @p shard_id
-inline inode_t shard_first_inode(unsigned shard_id) noexcept {
-    return shard_id;
+// Returns first inode (lowest by value) belonging to the shard @p fs_shard_id
+inline inode_t shard_first_inode(fs_shard_id_t fs_shard_id) noexcept {
+    return fs_shard_id;
 }
+
+class shard_inode_allocator {
+    fs_shard_id_t _fs_shards_pool_size;
+    fs_shard_id_t _fs_shard_id;
+    std::optional<inode_t> _latest_allocated_inode;
+
+public:
+    shard_inode_allocator(fs_shard_id_t fs_shards_pool_size, fs_shard_id_t fs_shard_id, std::optional<inode_t> latest_allocated_inode = std::nullopt)
+    : _fs_shards_pool_size(fs_shards_pool_size)
+    , _fs_shard_id(fs_shard_id)
+    , _latest_allocated_inode(latest_allocated_inode) {}
+
+    inode_t alloc() noexcept {
+        if (not _latest_allocated_inode) {
+            _latest_allocated_inode = shard_first_inode(_fs_shard_id);
+            return *_latest_allocated_inode;
+        }
+
+        _latest_allocated_inode = shard_next_inode(*_latest_allocated_inode, _fs_shards_pool_size);
+        return *_latest_allocated_inode;
+    }
+
+    void reset(std::optional<inode_t> latest_allocated_inode = std::nullopt) noexcept {
+        _latest_allocated_inode = latest_allocated_inode;
+    }
+};
 
 } // namespace seastar::fs
