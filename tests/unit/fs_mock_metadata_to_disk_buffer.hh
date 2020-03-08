eacddb5aee7b3f5d39ee7eab58cc9c414180a203
Michał Niciejewski <quport@gmail.com>
2020-03-09 00:20:28 +0100

    fs: mock_metadata_to_disk_buffer: change MAX_ELEMENTS_NUMBER to MAX_ELEMENTS_PRINTED

--- tests/unit/fs_mock_metadata_to_disk_buffer.hh
+++ tests/unit/fs_mock_metadata_to_disk_buffer.hh
@@ -454,15 +454,15 @@ std::ostream& operator<<(std::ostream& os, const ondisk_rename_dir_entry_header&
 }
 
 std::ostream& operator<<(std::ostream& os, const temporary_buffer<uint8_t>& data) {
-    constexpr size_t MAX_ELEMENTS_NUMBER = 10;
-    for (size_t i = 0; i < std::min(MAX_ELEMENTS_NUMBER, data.size()); ++i) {
+    constexpr size_t MAX_ELEMENTS_PRINTED = 10;
+    for (size_t i = 0; i < std::min(MAX_ELEMENTS_PRINTED, data.size()); ++i) {
         if (isprint(data[i]) != 0) {
             os << data[i];
         } else {
             os << "<" << (int)data[i] << ">";
         }
     }
-    if (data.size() > MAX_ELEMENTS_NUMBER) {
+    if (data.size() > MAX_ELEMENTS_PRINTED) {
         os << "...";
     }
     return os;
